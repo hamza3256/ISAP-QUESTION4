77d1a185aceb34b8885fd9ed98aa06d648e9430a
hamza3256 <hamza3256@live.co.uk>
2022-03-03 18:01:57 +0000

    Question 4 completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const topFive = (file) => {
+    const occurrences = {};
+
+    var lines = file.split("\n");
+    for (var i = 0; i < lines.length; i++) {
+      if (lines[i] in occurrences) {
+        occurrences[lines[i]]++;
+      } else {
+        occurrences[lines[i]] = 1;
+      }
+    }
+
+    var vals = Object.keys(occurrences).map((key) => {
+      return [key];
+    });
+
+    vals.sort((first, second) => {
+      return second[1] - first[1];
+    });
+
+    return vals.slice(0, 5);
+  };
+
+  const handleFileChosen = (file) => {
+    const fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      setItems(topFive(e.target.result));
+    };
+    fileReader.readAsText(file);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Please upload a text file</h1>
+      <div className='upload-expense'>
+        <input
+          type='file'
+          id='file'
+          className='input-file'
+          accept='.txt'
+          onChange={(e) => handleFileChosen(e.target.files[0])}
+        />
+      </div>
+      <h2>Result:</h2>
+      <div>
+        <ul style={{ listStyleType: "none" }}>
+          {items.map((i) => (
+            <li key={i}>{i}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
